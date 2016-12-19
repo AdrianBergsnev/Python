274e7a632ae2104e1cb2bcd101f19ca6da7e561c
Adrian Bergsnev <adrian.bergsnev@gmail.com>
2016-12-19 12:45:24 +0100

    Commit
    Edited spaces

--- Auto Config Cisco.py
+++ Auto Config Cisco.py
@@ -7,18 +7,14 @@ Switch = {
 	'password': 'P@ssw0rd',
 }
 
-
 conn = ConnectHandler(**Switch)
-
 
 vlan_database = [ 'interface vlan 1', 
 				  'ip address 192.168.20.1 255.255.255.0',
 				  'interface vlan 10',
 				  'interface vlan 20',
 				  'interface vlan 30',]				  
-				  
-
-
+				 
 configure_interfaces = [ 'interface fastethernet0/1',
 			 		   'switchport mode access',
 			 		   'switchport access vlan 1',
@@ -43,10 +39,7 @@ configure_interfaces = [ 'interface fastethernet0/1',
 			 		   'switchport mode trunk',
 			 		   'switchport trunk allowed vlan 1,10,20,30',
 			 		   'no shut']
-			 		  		 		  
-			 
-
-
+			 		  		 		  		
 conn.send_config_set(vlan_database)
 conn.send_config_set(configure_interfaces)
 
