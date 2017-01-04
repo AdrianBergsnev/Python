99ec9ede255a220e34f58d9fe5fd637790759585
Adrian Bergsnev <adrian.bergsnev@gmail.com>
2017-01-04 12:43:24 +0100

    Added comments for each line

--- Auto Backup.py
+++ Auto Backup.py
@@ -2,14 +2,26 @@ from netmiko import ConnectHandler
 
 from Device_list import devices
 
+#Importing devices from another python script
+
 for device in devices:
 
+#foreach device in devices, the var contains multiple devices
 
 	conn = ConnectHandler(**device)	
 
+#Connecting to the devices thats in the var
+
 	out = conn.send_command_timing('copy running-config tftp://192.168.0.221/')
+
+#Sending command, and expects a following answer
+
 	out += conn.send_command_timing("")
 	out += conn.send_command_timing("")
+
+#Sends the "Enter" keystroke to the commandline
 
 
 	conn.disconnect()
+
+#disconnects the SSH session
