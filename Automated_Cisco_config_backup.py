4b96ae9313256842599fbbd1d43a8e741948807d
Adrian Bergsnev <adrian.bergsnev@gmail.com>
2017-01-05 09:36:35 +0100

    Added comments for password prompting

--- Automated_Cisco_config_backup.py
+++ Automated_Cisco_config_backup.py
@@ -14,23 +14,27 @@ def get_input(prompt=''):
 		line = input(prompt)
 	return line
 
+#Prompting 
+
 password = get_input('Password: ')
 
+#Defined password prompt for device connection
 
 with open('devices.json') as dev_file:
 		devices = json.load(dev_file)
 
-#Importing devices from another python script
+#Importing devices from another .json
 
 for device in devices:
 
-	device['password'] = password
-
 #foreach device in devices, the var contains multiple devices
+
+	device['password'] = password
+#Defined password in connection windows before connection to device
 
 	conn = ConnectHandler(**device)	
 
-#Connecting to the devices thats in the var
+#Connecting to the devices imported from .json
 
 	out = conn.send_command_timing('copy running-config tftp://192.168.0.221/')
 
@@ -39,7 +43,7 @@ for device in devices:
 	out += conn.send_command_timing("")
 	out += conn.send_command_timing("")
 
-#Sends the "Enter" keystroke to the commandline
+#Sends no keyboard strokes = continues proceeding tftp confirm
 
 	conn.disconnect()
 
